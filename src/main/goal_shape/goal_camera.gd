49474bc23d0ac6d07372711c604244945ca267a9
voxelv <slippytim@gmail.com>
2019-10-05 11:45:16 -0700

    goal viewer!

---
+++ src/main/goal_shape/goal_camera.gd
@@ -0,0 +1,30 @@
+extends Spatial
+
+export (float, 0.0, 2.0) var rotation_speed = PI/2
+
+# zoom settings
+export (float) var max_zoom = 3.0
+export (float) var min_zoom = 0.4
+export (float, 0.05, 1.0) var zoom_speed = 0.5
+
+var zoom = (max_zoom - min_zoom) / 2.0 + min_zoom
+
+var time_acc = 0
+
+func set_zoom(z:float):
+	zoom = z
+	zoom = clamp(zoom, min_zoom, max_zoom)
+
+func _process(delta):
+	rotate_object_local(Vector3.UP, rotation_speed * delta)
+	
+	time_acc += delta
+	scale = lerp(scale, Vector3.ONE * zoom, zoom_speed)
+	
+	$inner_gimbal.rotation.x = sin(time_acc)
+
+func _on_increase_zoom_button_pressed():
+	set_zoom(zoom - zoom_speed)
+
+func _on_decrease_zoom_button_pressed():
+	set_zoom(zoom + zoom_speed)
